a0cd91e56e0990a849274ef2ce6f14f938fb794a
Nathan Griffith <originalsmudge@gmail.com>
2010-05-09 15:42:00 -0400

    created empty java files for us to start using

---
+++ User.java
@@ -0,0 +1,13 @@
+/*  Ahmet Aktay and Nathan Griffith
+ *  DarkChat
+ *  CS435: Final Project
+ */
+import java.io.*;
+import java.net.*;
+import java.lang.Math;
+
+
+public class User {
+
+} // end of class
+
