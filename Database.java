93d4493f58dca4e5bb9159901848b8dfb7478b61
Nathan Griffith <originalsmudge@gmail.com>
2010-05-09 18:38:22 -0400

    added db code (not really working)

--- Database.java
+++ Database.java
@@ -2,12 +2,56 @@
  *  DarkChat
  *  CS435: Final Project
  */
-import java.io.*;
-import java.net.*;
-import java.lang.Math;
+import java.sql.*;
 
 
 public class Database {
+
+    //Database connection
+    Connection conn;
+    PreparedStatement prep;
+    Statement stat;
+	
+    //Constructor
+    public Database() {
+      Class.forName("org.sqlite.JDBC");
+      conn = DriverManager.getConnection("jdbc:sqlite:test.db");
+      stat = conn.createStatement();
+      stat.executeUpdate("drop table if exists people;");
+      prep = conn.prepareStatement("insert into ? values (?, ?);");
+      stat.executeUpdate("create table people (name, occupation);");
+
+    }
+	
+    public void setThing() {
+      prep.setString(1, "Gandhi");
+      prep.setString(2, "politics");
+      prep.addBatch();
+      prep.setString(1, "Turing");
+      prep.setString(2, "computers");
+      prep.addBatch();
+      prep.setString(1, "Wittgenstein");
+      prep.setString(2, "smartypants");
+      prep.addBatch();
+      
+      conn.setAutoCommit(false);
+      prep.executeBatch();
+      conn.setAutoCommit(true);
+    }
+    
+    public void get() {
+      ResultSet rs = stat.executeQuery("select * from people;");
+      while (rs.next()) {
+        System.out.println("name = " + rs.getString("name"));
+        System.out.println("job = " + rs.getString("occupation"));
+      }
+      rs.close();
+    }
+    
+    public void close() {
+      conn.close();
+    }
+  
 
 } // end of class
 
