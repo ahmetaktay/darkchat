334dc97c98117aed1d5f96bfab4c3c938bb5545a
Ahmet Aktay <ahmet@dhcp128036171096.med.yale.edu>
2010-05-09 22:21:26 -0400

    a few methods for user & session creation

--- Session.java
+++ Session.java
@@ -4,10 +4,30 @@
  */
 import java.io.*;
 import java.net.*;
+import java.util.Date;
 import java.lang.Math;
 
 
 public class Session {
-
+	public InetSocketAddress address;
+	public User user;
+	public Date lastValid;
+	
+	Session(User user, String hostName, int port)
+	{
+		this(user, new InetSocketAddress(hostName,port));
+	}
+	Session(User user, InetSocketAddress inetSocketAddress)
+	{
+		this(user, inetSocketAddress, new Date());
+	}
+	Session(User user, InetSocketAddress inetSocketAddress, Date lastValid)
+	{
+		this.user = user;
+		this.address = inetSocketAddress;
+		this.lastValid = lastValid;
+	}
+	
+	
 } // end of class
 
--- User.java
+++ User.java
@@ -4,10 +4,29 @@
  */
 import java.io.*;
 import java.net.*;
+import java.util.ArrayList;
 import java.lang.Math;
+
+import com.sun.tools.javac.util.List;
 
 
 public class User {
-
+	public String name;
+	public ArrayList<Session> sessions;
+	
+	public User()
+	{
+		this("Anonymous");
+	}
+	public User(String userName)
+	{
+		this(userName,new ArrayList<Session>());
+	}
+	public User(String userName, ArrayList<Session> sessionList)
+	{
+		this.name = userName;
+		this.sessions = sessionList;
+	}
+	
 } // end of class
 
