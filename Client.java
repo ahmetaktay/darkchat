797645611a8c0c1e96c710a476fc519a81f9a534
Nathan Griffith <originalsmudge@gmail.com>
2010-05-09 20:54:29 -0400

    removed imports

--- Client.java
+++ Client.java
@@ -2,10 +2,6 @@
  *  DarkChat
  *  CS435: Final Project
  */
-import java.io.*;
-import java.net.*;
-import java.lang.Math;
-
 
 public class Client {
 	UserList everyone = UserList.loadFromDatabase;
