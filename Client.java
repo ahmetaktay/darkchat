310eb62563ebd3e99113a0e6fb5d63c5f87e64c9
Smudge <originalsmudge@gmail.com>
2010-05-11 16:10:53 -0400

    added listener and reciever

--- Client.java
+++ Client.java
@@ -4,16 +4,21 @@
  */
 
 public class Client {
-	static String usage = "-port [port_num:int]";
+	static String usage = "-p [port_num:int] -t [nthreads:int]";
 	public static void main(String[] args) throws Exception {
     // Defaults:
     int localPort = 6789;
+    int nthreads = 100;
 
     //Iterate through args
     for (int i = 0; i < args.length; i++) {
-      if (args[i].equals("-port")) {
+      if (args[i].equals("-p")) {
         i++;
         localPort = Integer.parseInt(args[i]);
+      }
+      if (args[i].equals("-t")) {
+        i++;
+        nthreads = Integer.parseInt(args[i]);
       }
       else {
         System.out.println(usage);
@@ -21,6 +26,9 @@ public class Client {
       }
     }
   
+    //Start the listener thread
+    Thread listener = new Thread(new Listener(localPort,nthreads));
+    listener.start();
   
     //Set-up the essentials
     Database db = new Database(); 
