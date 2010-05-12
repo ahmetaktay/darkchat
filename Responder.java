2aa6782ce8522121802706dfd5585424dd4f9d4c
Smudge <originalsmudge@gmail.com>
2010-05-12 07:09:59 -0400

    couple tweaks

--- Responder.java
+++ Responder.java
@@ -155,15 +155,15 @@ class Responder implements Runnable {
         else if (ln.equals("BUD")) // someone is delivering known users
         {
         // TODO: check if we requested one
-          //      String message = String.format("BUD\n%s\n%s\n", localUser.name, port, ofUserName);
-//            String fromUserName = inFromClient.readLine();
-//            String ofUserName = inFromClient.readLine();
-//            port = Integer.parseInt(inFromClient.readLine());
-//            Boolean friends = false;
-//            synchronized (knownUsers) {
-//                fromUser = knownUsers.get(fromUserName,true);
-//                ofUser          = knownUsers.get(ofUserName,true);
-//            }
+/*        String message = String.format("BUD\n%s\n%s\n", localUser.name, port, ofUserName);
+          String fromUserName = inFromClient.readLine();
+          String ofUserName = inFromClient.readLine();
+          port = Integer.parseInt(inFromClient.readLine());
+          Boolean friends = false;
+          synchronized (knownUsers) {
+          fromUser = knownUsers.get(fromUserName,true);
+          ofUser          = knownUsers.get(ofUserName,true);
+          }*/
           String fromUserName = inFromClient.readLine();
           port = Integer.parseInt(inFromClient.readLine());
           String ofUserName = inFromClient.readLine();
@@ -184,25 +184,22 @@ class Responder implements Runnable {
           else
           {
             MyUtils.dPrintLine("Received valid BUDs");
-            MyUtils.dPrintLine(String.format("%s sent %s's knowns, delivering them via BUD.", fromUser, ofUser));
+            MyUtils.dPrintLine(String.format("%s sent %s's knowns, delivering them via BUD.", fromUser.name, ofUser.name));
             synchronized(ofUser)
             {
               synchronized(knownUsers)
               {
                 String budName = inFromClient.readLine();
                 while(budName != "")
-                {        
+                {
                   User budUser = knownUsers.get(budName);
-                  synchronized (budUser) {
-                    ofUser.meetUser(budUser);
-                  }
+                  ofUser.meetUser(budUser);
                   budName = inFromClient.readLine();
                 }
                   
               }
             }
           }
-
         }
         else
           MyUtils.dPrintLine("Unrecognized message format");
